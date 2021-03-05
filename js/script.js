c89bd8f1bd2653eff812b3983440800f281b1378
RAZOR <alimnurarman@gmail.com>
2021-03-05 14:19:22 +0600

    first commit

---
+++ js/script.js
@@ -0,0 +1,32 @@
+/* Задания на урок:
+
+1) Удалить все рекламные блоки со страницы (правая часть сайта)
+
+2) Изменить жанр фильма, поменять "комедия" на "драма"
+
+3) Изменить задний фон постера с фильмом на изображение "bg.jpg". Оно лежит в папке img.
+Реализовать только при помощи JS
+
+4) Список фильмов на странице сформировать на основании данных из этого JS файла.
+Отсортировать их по алфавиту 
+
+5) Добавить нумерацию выведенных фильмов */
+
+'use strict';
+
+const movieDB = {
+    movies: [
+        "Логан",
+        "Лига справедливости",
+        "Ла-ла лэнд",
+        "Одержимость",
+        "Скотт Пилигрим против..."
+    ]
+};
+movieDB.innerHTML = `<ol>${movies}</ol>`;
+
+// Get started
+
+let mainBg = document.querySelector('.promo__bg');
+mainBg.style.backgroundImage = "url('img/bg.jpg')";
+// mainBg.style.cssText = "background: url('img/bg.jpg') center center/cover no-repeat;background-position:top;padding:107px 0 0 61px}";
